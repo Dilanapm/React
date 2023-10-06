f667ebdc5c2ba2d2764e5f367f211f3a20aa1acb
Dilanapm <apolacadilan@gmail.com>
2023-10-06 14:07:11 -0400

    first commit

---
+++ src/assets/carsAPI.js
@@ -0,0 +1,81 @@
+
+export const cars = [
+  {
+    title: "Sedans",
+    description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    imgUrl: "/icon-sedans.svg",
+    theme: "orange",
+  },
+  {
+    title: "Suvs",
+    description:"Take an SUV for its spacious interior, power and veraitily. Perfect for your next faminily vacation and off-road adventures.",
+    imgUrl: "/icon-suvs.svg",
+    theme: "green",
+  },
+  {
+    title: "Luxury",
+    description:"Cruise in the best car brands without the bloaoated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    imgUrl: "/icon-luxury.svg",
+    theme: "dark-green",
+  },
+  {
+    title: "Warrior",
+    description:"Choose this service if you think that being a warrior and fighting for others is a good idea.",
+    imgUrl: "/icon-warrio.svg",
+    theme: "red",
+  },
+  {
+    title: "Owner",
+    description:"Choose this service if you think it is better to drive on your own or you simply don't like the idea of ​​being with someone in the car.",
+    imgUrl: "/icon-own.svg",
+    theme: "violet",
+  },
+  {
+    title: "friendly",
+    description:"Choose this service if you don't want to cause problems, you are friendlier than the color pink.",
+    imgUrl: "/icon-friend.svg",
+    theme: "pink",
+  },
+  {
+    title: "God",
+    description:"If you ever wanted to be a god, this is your time.",
+    imgUrl: "/icon-god.svg",
+    theme: "orange",
+  },
+  {
+    title: "superhuman",
+    description:"Choose this service if you feel capable of dominating the entire world, plus you will face monsters.",
+    imgUrl: "/icon-superhuman.svg",
+    theme: "green",
+  },
+  {
+    title: "peaky blinders",
+    description:"now you can have your mafia gang and govern your area and carry many weapons.",
+    imgUrl: "/icon-own.svg",
+    theme: "dark-green",
+  },
+  {
+    title: "flash",
+    description:"If you are one of those people who fall behind, this will surely help you..",
+    imgUrl: "/icon-flash.svg",
+    theme: "red",
+  },
+  {
+    title: "joker",
+    description:"This service is for maniacs, please be careful when using it, and don't forget to laugh.",
+    imgUrl: "/icon-master.svg",
+    theme: "violet",
+  },
+  {
+    title: "tooler",
+    description:"Do you feel capable of solving any problem through this service If so, let's do it.",
+    imgUrl: "/icon-tooler.svg",
+    theme: "pink",
+  },
+  {
+    title: "sky",
+    description:"Now you are in heaven, let yourself be carried away by the sky service that will take you to your destination without any problem.",
+    imgUrl: "/icon-sky.svg",
+    theme: "red",
+  }
+]
